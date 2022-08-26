08f0e8ae09739f349da6eb6b9918c6c5cbbfa660
Andres Varon <anvaron@gmail.com>
2022-08-26 11:37:59 -0400

    Project update

--- back-end/validations/checkData.js
+++ back-end/validations/checkData.js
@@ -2,7 +2,7 @@
 // *****************************************
 //   G E N E R A L  V A L I D A T I O N S
 // *****************************************
-//
+// Validate song's names
 const checkSong = (req, res, next) => {
   console.log('name is being checked');
   if (req.body.name) {
@@ -13,7 +13,7 @@ const checkSong = (req, res, next) => {
   }
 };
 
-// 
+// Validate artist's names
 const checkArtist = (req, res, next) => {
   console.log('artist is being checked');
   if (req.body.artist) {
@@ -24,7 +24,7 @@ const checkArtist = (req, res, next) => {
   }
 };
 
-// 
+// Validate boolean values
 const checkBoolean = (req, res, next) => {
   if (req.body.is_favorite) {
     next();
